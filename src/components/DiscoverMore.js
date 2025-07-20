d2abe06bc98b9d792388e87a281c37218c63aa3c
Aya-Farahat <labaya665@gmail.com>
2025-07-20 03:38:47 +0300

    Update DiscoverMore.js

--- src/components/DiscoverMore.js
+++ src/components/DiscoverMore.js
@@ -8,10 +8,12 @@ const DiscoverMore = () => {
         position: "relative",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        overflow: "hidden",
+        overflow: "visible",
+        minHeight: "auto",
+        height: "auto",
       }}
     >
-      <div className="container" style={{ padding: "200px 0 50px" }}>
+      <div className="container discover-container" style={{ padding: "100px 100px 20px 0px" }}>
         <div className="d-flex flex-lg-row align-items-start justify-content-evenly">
           {/* النص */}
           <motion.div
@@ -26,9 +28,10 @@ const DiscoverMore = () => {
               style={{
                 fontFamily: "'IBM Plex Serif', serif",
                 fontWeight: 700,
-                fontSize: "36px",
+                fontSize: "25px",
                 lineHeight: "136%",
                 letterSpacing: "6px",
+                color: "#193050",
               }}
             >
               Discover More
@@ -86,22 +89,44 @@ const DiscoverMore = () => {
               </span>
             </a>
 
-            <div
-              className="position-absolute d-none d-lg-block"
-              style={{ left: "-140px", bottom: "70px" }}
+            {/* الصورة للجوال - خلف النص */}
+            <div 
+              className="position-absolute d-block d-lg-none"
+              style={{ 
+                right: "-90px", 
+                bottom: "0px",
+                zIndex: -1
+              }}
             >
               <img
                 src="/images/discsub.png"
                 alt="discover img"
-                className=" img-fluid mt-4"
-                style={{ width: "500px", height: "auto" }}
+                className="img-fluid"
+                style={{ 
+                  width: "800px", 
+                  height: "auto",
+                  
+                }}
+              />
+            </div>
+
+            {/* الصورة للكمبيوتر */}
+            <div
+              className="position-absolute d-none d-lg-block"
+              style={{ left: "-140px", bottom: "0px", marginLeft: "150px" }}
+            >
+              <img
+                src="/images/discsub.png"
+                alt="discover img"
+                className="img-fluid mt-4"
+                style={{ width: "300px", height: "auto" }}
               />
             </div>
           </motion.div>
 
-          {/* الصور */}
+          {/* الصور الأربع - تظهر فقط في الكمبيوتر */}
           <motion.div
-            className="d-none d-lg-flex ps-lg-4"
+            className="d-none d-lg-flex"
             style={{ gap: "1rem", flexShrink: 0 }}
             initial={{ opacity: 0, x: 100 }}
             whileInView={{ opacity: 1, x: 0 }}
@@ -113,21 +138,39 @@ const DiscoverMore = () => {
               <img
                 src="/images/Discover/disc1.png"
                 alt="Al Olaya"
-                className="img-fluid rounded shadow-sm mb-3"
+                className="img-fluid shadow-sm"
                 style={{
                   width: "180px",
                   height: "250px",
                   objectFit: "cover",
+                  margin: 0,
+                  padding: 0,
                 }}
               />
+
+              <span
+                style={{
+                  color: "#5584b1ff",
+                  fontWeight: "550",
+                  fontSize: "17px",
+                  margin: 0,
+                  padding: 0,
+                }}
+              >
+                Al Olaya
+              </span>
+
               <img
-                src="/images/Discover/disc2.png"
+                src="/images/Discover/disc3.png"
                 alt="Al Olaya"
-                className="img-fluid rounded"
+                className="img-fluid"
                 style={{
                   width: "180px",
-                  height: "250px",
+                  height: "125px",
                   objectFit: "cover",
+                  objectPosition: "bottom",
+                  margin: 0,
+                  padding: 0,
                 }}
               />
             </div>
@@ -137,27 +180,51 @@ const DiscoverMore = () => {
               <img
                 src="/images/Discover/disc4.png"
                 alt="Al Olaya"
-                className="img-fluid rounded shadow-sm mb-3"
+                className="img-fluid shadow-sm"
                 style={{
                   width: "180px",
                   height: "250px",
                   objectFit: "cover",
+                  margin: 0,
+                  padding: 0,
                 }}
               />
+              <span
+                style={{
+                  color: "#5584b1ff",
+                  fontWeight: "550",
+                  fontSize: "17px",
+                  margin: 0,
+                  padding: 0,
+                }}
+              >
+                Plumeria
+              </span>
+
               <img
-                src="/images/Discover/disc3.png"
+                src="/images/Discover/disc2.png"
                 alt="Al Olaya"
-                className="img-fluid rounded shadow-sm mb-3"
+                className="img-fluid shadow-sm"
                 style={{
                   width: "180px",
-                  height: "250px",
+                  height: "63px",
                   objectFit: "cover",
+                  objectPosition: "center",
+                  margin: 0,
+                  padding: 0,
                 }}
               />
             </div>
           </motion.div>
         </div>
       </div>
+      <style>{`
+        @media (max-width: 768px) {
+          .discover-container {
+            padding: 100px 100px 250px 0px !important;
+          }
+        }
+      `}</style>
     </div>
   );
 };
